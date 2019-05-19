9fea00366b3694a69b2fa7736796d3c2f3d16d6b
Arockiasamy, Viniston <Viniston.Arockiasamy@aspect.com>
2019-05-19 21:41:23 +0530

    Solution changes

---
+++ BusinessDataAccess/EF/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BusinessDataAccessDefinition.Models;
+
+namespace BusinessDataAccess.EF
+{
+    public static class DbInitializer
+    {
+
+        public static void Initialize(ReviewDbContext context)
+        {
+            context.Database.EnsureCreated();
+            var skill = new Review { Id = 0, Name = $"Test Review-{DateTime.Now.Ticks}" };
+            context.Review.Add(skill);
+        }
+    }
+}
---
+++ BusinessDataAccess/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DataModels = BusinessDataAccessDefinition.Models;
+using Entities = BusinessEntitties;
+
+namespace BusinessDataAccess.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region Review
+
+            CreateMap<Entities.Review, DataModels.Review>();
+
+            CreateMap<DataModels.Review, Entities.Review>();
+
+            #endregion
+        }
+    }
+}
